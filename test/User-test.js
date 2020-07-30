f262676c865f3732fe5fa88f67aa5cfef87a9faf
Ben McClung <aurumvalian@gmail.com>
2020-07-30 09:44:40 -0600

    Build user test skeleton

---
+++ test/User-test.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+import User from '../src/User.js';
+const expect = chai.expect;
+
+describe('User', () => {
+  before(() => {
+    const user = new User();
+  })
+  it('should be a function', () => {
+    expect()
+  })
+})
