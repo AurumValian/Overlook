bed436e709f8f83de02a8a84c23cb37fc86b7d02
Ben McClung <aurumvalian@gmail.com>
2020-07-30 09:50:40 -0600

    Add sample data to user test file

--- test/User-test.js
+++ test/User-test.js
@@ -4,9 +4,15 @@ const expect = chai.expect;
 
 describe('User', () => {
   before(() => {
+    const users = {"users":
+    [{"id": 1, "name": "Leatha Ullrich"},
+    {"id": 2, "name": "Rocio Schuster"},
+    {"id": 3, "name": "Kelvin Schiller"}]
+  };
     const user = new User();
   })
+
   it('should be a function', () => {
-    expect()
+    expect(User).to.be.a('function');
   })
 })
