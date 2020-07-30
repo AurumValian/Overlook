13a91d089c2de9ff6adda8537dc9f5c2f3460182
Ben McClung <aurumvalian@gmail.com>
2020-07-30 10:04:22 -0600

    Build user property tests

--- test/User-test.js
+++ test/User-test.js
@@ -9,10 +9,22 @@ describe('User', () => {
     {"id": 2, "name": "Rocio Schuster"},
     {"id": 3, "name": "Kelvin Schiller"}]
   };
-    const user = new User();
+    const user = new User(users.users[0]);
   })
 
   it('should be a function', () => {
     expect(User).to.be.a('function');
+  })
+
+  it('should instantiate a user object', () => {
+    expect(user).to.be.an.instanceOf(User);
+  })
+
+  it('should have an id', () => {
+    expect(user.id).to.equal(1);
+  })
+
+  it('should have a name', () => {
+    expect(user.name).to.equal("Leatha Ullrich");
   })
 })
