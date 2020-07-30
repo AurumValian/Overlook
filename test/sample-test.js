fc6fb43e31feb2986be53af4ddcdb6e95a1c8d32
Ben McClung <aurumvalian@gmail.com>
2020-07-29 18:11:29 -0600

    Initial Commit

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,8 @@
 import chai from 'chai';
 const expect = chai.expect;
 
+//here we go!
+
 describe('See if the tests are running', function() {
   it('should return true', function() {
     expect(true).to.equal(true);
