37a58d4202dcff14f1cc13548a563bcaa24413e5
Ben McClung <aurumvalian@gmail.com>
2020-07-30 10:41:42 -0600

    Test user sayHello method

--- test/User-test.js
+++ test/User-test.js
@@ -9,7 +9,7 @@ describe('User', () => {
     {"id": 2, "name": "Rocio Schuster"},
     {"id": 3, "name": "Kelvin Schiller"}]
   };
-    const user = new User(users.users[0]);
+    let user = new User(users.users[1]);
   })
 
   it('should be a function', () => {
@@ -21,10 +21,20 @@ describe('User', () => {
   })
 
   it('should have an id', () => {
-    expect(user.id).to.equal(1);
+    expect(user.id).to.equal(2);
   })
 
   it('should have a name', () => {
+    expect(user.name).to.equal("Rocio Shuster");
+  })
+
+  it('should be able to have a different id and name', () => {
+    user = new User(user.users[0]);
+    expect(user.id).to.equal(1);
     expect(user.name).to.equal("Leatha Ullrich");
+  })
+
+  it('should be able to return a string with the name', () => {
+    expect(user.sayHello()).to.equal("Hello, Rocio Shuster!");
   })
 })
