c60cf3628e96f6ab6310bb95f5282f6e4e721b4d
Ben McClung <aurumvalian@gmail.com>
2020-07-30 23:54:02 -0600

    Fix Manager super

--- src/Manager.js
+++ src/Manager.js
@@ -1,4 +1,22 @@
 import User from './User';
 class Manager extends User {
+  constructor() {
+    super({id: "manager", name: "Overlook Manager"})
+  }
 
+  availableRooms() {
+
+  }
+
+  totalRevenue() {
+
+  }
+
+  availableRoomPercentage() {
+
+  }
+
+  searchUsersByName(name) {
+
+  }
 }
