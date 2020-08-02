d61ff14b51c894aac33f69dc8fb67c00a0f92664
Ben McClung <aurumvalian@gmail.com>
2020-08-02 13:46:20 -0600

    Build Rooms class

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,18 +1,29 @@
 class Rooms {
   constructor(rooms) {
-
+    this.rooms = rooms;
   }
 
-  availableRoomsToday() {
-
+  availableRooms(bookings, date) {
+    return this.rooms.filter(room => {
+      return bookings.reduce((isAvailable, booking) => {
+        if (room.number === booking.roomNumber && date === booking.date) {
+          return false;
+        } else {
+          return isAvailable;
+        }
+      }, true)
+    })
   }
 
   roomsOccupiedPercentage() {
-
+    return Math.round(this.availableRoomsToday(loadData.bookings).length / this.rooms.length);
   }
 
-  searchByType() {
-
+  searchByType(type, date, bookings) {
+    return this.rooms.filter(room => {
+      const roomsAvailable = availableRooms(bookings, date);
+      return (room.roomType === type && roomsAvailable.includes(room));
+    })
   }
 }
 
