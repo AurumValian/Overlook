c95b3bcb14e95d3541f8b89ad2fca6eb144aaa13
Ben McClung <aurumvalian@gmail.com>
2020-08-02 16:14:27 -0600

    Build manager totalRevenue method and test

--- src/Manager.js
+++ src/Manager.js
@@ -4,16 +4,16 @@ class Manager extends User {
     super({id: "manager", name: "Overlook Manager"})
   }
 
-  availableRooms() {
-
-  }
-
-  totalRevenue() {
-
-  }
-
-  availableRoomPercentage() {
-
+  totalRevenue(rooms, bookings, date) {
+    const bookingsOnDate = bookings.filter(booking => {
+      return booking.date === date;
+    })
+    return bookingsOnDate.reduce((totalRevenue, booking) => {
+      const roomOfBooking = rooms.find(room => {
+        return booking.roomNumber === room.number;
+      })
+      return roomOfBooking.costPerNight + totalRevenue;
+    }, 0)
   }
 
   searchUsersByName(name) {
--- test/Manager-test.js
+++ test/Manager-test.js
@@ -17,18 +17,18 @@ describe('Manager', () => {
   };
   manager = new Manager();
   customer = new Customer(users.users[0]);
-  bookings = {"bookings": [
+  bookings = [
     {"id":"5fwrgu4i7k55hl6x8","userID":1,"date":"2020/01/11","roomNumber":20,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl72q","userID":1,"date":"2020/01/19","roomNumber":19,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl72u","userID":3,"date":"2020/02/17","roomNumber":4,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl72v","userID":32,"date":"2020/01/25","roomNumber":9,"roomServiceCharges":[]}
-  ]}
-  rooms = {"rooms": [
+  ]
+  rooms = [
     {"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},
     {"number":4,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":429.44},
     {"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.17},
     {"number":6,"roomType":"junior suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":397.02}
-  ]}
+  ]
 });
   it('should be a function', () => {
     expect(Manager).to.be.a('function');
@@ -44,6 +44,12 @@ describe('Manager', () => {
 
   it('should have a name of Overlook Manager', () => {
     expect(manager.name).to.equal("Overlook Manager");
+  })
+
+  it('should be able to see total revenue for a given day', () => {
+    const date = "2020/02/17";
+    const revenue = manager.totalRevenue(rooms, bookings, date);
+    expect(revenue).to.equal(429.44);
   })
 
   it('should be able to search for customers by name', () => {
